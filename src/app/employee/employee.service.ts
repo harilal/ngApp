486887c819ecfe82f5cfe7a4f61da2ea799badcb
Harilal <logicmania@gmail.com>
2017-06-30 14:32:33 +0530

    new config

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
+import{ base_url } from "app/config";
 import 'rxjs/add/operator/map';
 @Injectable()
 export class EmployeeService {
-  base_url = "https://logicapi.herokuapp.com/api/";
+
 
   constructor(private http: Http) { }
   getEmployees() {
-    return this.http.get(this.base_url + "employee").map((data: Response) => {
+    return this.http.get(base_url + "employee").map((data: Response) => {
       return data.json()
     });
   }
--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
+
+import{ base_url } from "app/config";
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProjectService {
-  base_url = "https://logicapi.herokuapp.com/api/";
+
 
   constructor(private http: Http) { }
   getProjects() {
-    return this.http.get(this.base_url + "project").map((data: Response) => {
+    return this.http.get(base_url + "project").map((data: Response) => {
       return data.json()
     });
   }
